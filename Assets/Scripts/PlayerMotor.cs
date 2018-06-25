2bbf1f2ff3fbb59b6d52e4eb989f39bce53bbb46
Christopher D Koon <CHRKOON@uat.edu>
2018-06-24 20:59:14 -0700

    Milestone 1 Complete

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+    public int playerMoveSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(0,Input.GetAxis("Vertical") * Time.deltaTime * playerMoveSpeed, 0);
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * playerMoveSpeed, 0, 0);
+    }
+}
